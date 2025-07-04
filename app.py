237a081e11c0df0a063270cd7257556aa3017634
Azissaputra02 <azis.saputra2001@gmail.com>
2025-07-04 10:19:50 +0700

    1

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ if st.session_state.page == "Home":
         text-align: left;
         margin-top: -20px;
     ">
-    FP Knowledge Center
+    Financial planning
     </h1>
     """, unsafe_allow_html=True)
 
